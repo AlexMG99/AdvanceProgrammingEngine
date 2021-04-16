84b8baf063e6968c3386f926a9bd64a951fd3145
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2021-04-16 21:10:29 +0200

    Draw simple images with shader

--- Engine/Code/engine.h
+++ Engine/Code/engine.h
@@ -51,6 +51,24 @@ struct OpenGLInfo
     std::string* extensions;
 };
 
+struct VertexV3V2
+{
+    glm::vec3 pos;
+    glm::vec2 uv;
+};
+
+const VertexV3V2 vertices[] = {
+    { glm::vec3(-0.5, -0.5, 0.0), glm::vec2(0.0, 0.0)}, // bottom-left vertex
+    { glm::vec3(0.5, -0.5, 0.0), glm::vec2(1.0, 0.0)}, // bottom-left vertex
+    { glm::vec3(0.5, 0.5, 0.0), glm::vec2(1.0, 1.0)}, // bottom-left vertex
+    { glm::vec3(-0.5, 0.5, 0.0), glm::vec2(0.0, 1.0)}, // bottom-left vertex
+};
+
+const u16 indices[] = {
+    0, 1, 2,
+    0, 2, 3
+};
+
 struct App
 {
     // Loop

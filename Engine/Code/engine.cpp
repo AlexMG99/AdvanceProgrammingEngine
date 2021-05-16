e6fbd26343da1104bf3c8cb75482582ae433fa0a
Yessica Servin Dominguez <yessica.servindominguez@gmail.com>
2021-05-16 13:23:42 +0200

    changed clean color

--- Engine/Code/engine.cpp
+++ Engine/Code/engine.cpp
@@ -360,7 +360,7 @@ void Render(App* app)
             {
                 glBindFramebuffer(GL_FRAMEBUFFER, app->gBuffer);
                 // Draw function
-                glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
+                glClearColor(0.f, 0.f, 0.f, 1.0f);
                 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
                 glEnable(GL_DEPTH_TEST);
 
